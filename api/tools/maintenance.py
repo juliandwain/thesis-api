b4200ca840fd890a44ebb9e98615fbc0f8478a04
Julian Stang <julian.stang@tum.de>
2021-04-25 13:31:51 +0200

    Updated chapter initialization #2

---
+++ api/tools/maintenance.py
@@ -0,0 +1,240 @@
+# -*- coding: utf-8 -*-
+
+import pathlib
+import re
+import shutil
+import string
+from typing import Optional
+
+from .. import ENCODING, get_logger
+from .template_strings import (
+    ChapterTemplate,
+    SectionTemplate,
+    SubsectionTemplate,
+)
+
+COUNTER = 0
+INPUT_TEMPLATE = string.Template("\n\\input{$path}\n")
+LOGGER = get_logger(__name__)
+PATTERN = re.compile("(?<=input{).*?(?=})")
+TEX_FILE = ".tex"
+
+
+class Maintainer(object):
+    def __init__(self, thesis_dir: pathlib.Path) -> None:
+        """Init the class.
+
+        Parameters
+        ----------
+        thesis_dir : pathlib.Path
+            The path to the thesis directory.
+        
+        """
+        self._thesis_dir = thesis_dir
+        self._chapter_dir = thesis_dir / "chapters"
+        super().__init__()
+
+    def check_inputs(self, child: Optional[pathlib.Path] = None) -> None:
+        """Recursivley check ``child`` for \\input statements.
+
+        Parameters
+        ----------
+        child : Optional[pathlib.Path], optional
+            The file which should be checked for input statements,
+            by default None.
+        
+        """
+        for child in self._thesis_dir.iterdir():
+            if child.is_dir():
+                if any(child.iterdir()):
+                    self.check_inputs(child)
+            else:
+                if TEX_FILE in child.parts[-1]:
+                    inputs = self.find_input(child)
+                    for inp in inputs:
+                        if not inp.exists():
+                            COUNTER += 1
+                            LOGGER.warning(
+                                f"File {inp} is included in {child} but does not exist!\n"
+                            )
+
+    def cleanup(self, child=None, delete: bool = False) -> None:
+        """Cleanup empty folders.
+
+        Parameters
+        ----------
+        child : pathlib.Path, optional
+            The directory which is checked, by default None.
+        delete : bool, optional
+            Determine if empty folders should be deleted or only printed
+            to console for user notification, by default False.
+        
+        """
+        for child in self._thesis_dir.iterdir():
+            if child.is_dir():
+                if any(child.iterdir()):
+                    self.cleanup(child, delete)
+                else:
+                    LOGGER.debug(f"{child} is empty!")
+                    if delete:
+                        LOGGER.debug(f"{child} is deleted since {delete=}!\n")
+                        shutil.rmtree(child)
+                    else:
+                        LOGGER.debug(
+                            f"{child} is not deleted since {delete=}!\n"
+                        )
+            else:
+                continue
+
+    def create_ftc(self, path: pathlib.Path, typ: dict) -> None:
+        """Create the figs, tabs, and/or code directories.
+
+        Parameters
+        ----------
+        path : pathlib.Path
+            The path in which the ftc-direcotires should be created.
+        typ : dict
+            A dictionary containing information abouth which of the directories
+            should be created at a given level.
+
+        """
+        for k, v in typ.items():
+            temp_path = path / k
+            if v:
+                try:
+                    temp_path.mkdir(parents=True, exist_ok=False)
+                except FileExistsError:
+                    LOGGER.debug(f"Folder {temp_path} already exists.")
+            else:
+                LOGGER.debug(
+                    f"Folder {temp_path} was not created because it was set to {v}."
+                )
+
+    def find_input(self, path: pathlib.Path) -> list[pathlib.Path]:
+        """Find all ``\\input`` statements in a given file.
+
+        Parameters
+        ----------
+        path : pathlib.Path
+            The file which should be examined.
+
+        Returns
+        -------
+        list[pathlib.Path]
+            A list of all paths in input statements.
+
+        """
+        with path.open(mode="r", encoding=ENCODING) as file:
+            temp = file.read()
+            inputs = [pathlib.Path(p.group(0)) for p in PATTERN.finditer(temp)]
+        return inputs
+
+    def init_chapter_dir(self, chapter, sections, subsections) -> None:
+        # get the type of folder to create
+        chapter_type = next(iter(chapter))
+        # get the chapter number
+        chapter_num = str(chapter.pop("chapter", 10))
+        # define the chapter
+        chapter_ = chapter_type + chapter_num
+        # get the number of sections in the chapter
+        num_sections = chapter.pop("num_sections", 0)
+        # test if the number of sections aligns with the number of sections given
+        assert num_sections == (
+            n := len(sections)
+        ), f"Number of sections does not match!\nExpected: {num_sections}, Got: {n}!"
+        chapter_path = self._chapter_dir / chapter_
+        try:
+            chapter_path.mkdir(parents=True, exist_ok=False)
+        except FileExistsError as e:
+            LOGGER.critical(
+                f"{chapter_path} already exists!\nMaybe you want to create a new chapter?\n"
+            )
+            raise e
+        chapter_file = chapter_path / (chapter_ + TEX_FILE)
+        # create the chapter directories
+        self.create_ftc(chapter_path, chapter)
+        # open the chapter template
+        chapter_template = ChapterTemplate()
+        chapter_template_str = chapter_template.substitute(
+            title=chapter_, label=chapter_
+        )
+        if num_sections != 0:
+            sec_path = chapter_path / "sections"
+            sec_path.mkdir(parents=True, exist_ok=True)
+            for section in sections:
+                sec_type = next(iter(section))
+                sec_num = str(section.pop("section", 10))
+                sec_ = sec_type + sec_num
+                num_subsections = section.pop("num_subsections", 10)
+                assert num_subsections == (
+                    n := len(subsections[sec_num])
+                ), f"Number of subsections does not match!\nExpected: {num_subsections}, Got: {n}!"
+                sec_dir = sec_path / sec_
+                sec_dir.mkdir(parents=True, exist_ok=True)
+                sec_file = sec_dir / (sec_ + TEX_FILE)
+                # open the section template
+                sec_template = SectionTemplate()
+                sec_template_str = sec_template.substitute(
+                    title=f"{chapter_}-{sec_}", label=f"{chapter_}-{sec_}",
+                )
+                # create the section directories
+                self.create_ftc(sec_dir, section)
+                chapter_template_str += INPUT_TEMPLATE.substitute(
+                    path=f"{sec_file}".replace("\\", "/")
+                )
+                if num_subsections != 0:
+                    subsec_path = sec_dir / "subsections"
+                    subsec_path.mkdir(parents=True, exist_ok=True)
+                    for subsection in subsections[str(sec_num)]:
+                        subsec_type = next(iter(subsection))
+                        subsec_num = str(subsection.pop("subsection", 10))
+                        subsec_ = subsec_type + subsec_num
+                        subsec_dir = subsec_path / subsec_
+                        subsec_dir.mkdir(parents=True, exist_ok=True)
+                        subsec_file = subsec_dir / (subsec_ + TEX_FILE)
+                        # open the subsection template
+                        subsec_template = SubsectionTemplate()
+                        # create the subsection latex file
+                        subsec_template_str = subsec_template.substitute(
+                            title=f"{chapter_}-{sec_}-{subsec_}",
+                            label=f"{chapter_}-{sec_}-{subsec_}",
+                        )
+                        self.tex_file(
+                            subsec_file, subsec_template_str,
+                        )
+                        # create the subsection directories
+                        self.create_ftc(subsec_dir, subsection)
+                        sec_template_str += INPUT_TEMPLATE.substitute(
+                            path=f"{subsec_file}".replace("\\", "/")
+                        )
+                else:
+                    LOGGER.debug(f"No Subsections created in {sec_dir}!")
+                # create the section latex file
+                self.tex_file(sec_file, sec_template_str)
+
+        else:
+            LOGGER.debug(f"No Sections created in {chapter_path}!")
+        # create the chapter latex file
+        self.tex_file(chapter_file, chapter_template_str)
+
+    def tex_file(self, path: pathlib.Path, temp: str) -> None:
+        """Create the template LaTeX file.
+
+        Parameters
+        ----------
+        path : pathlib.Path
+            The file which should be created.
+        temp : str
+            The content to write to the file.
+
+        Notes
+        -----
+        - This function uses the templates provided from ``open_template``.
+        - If the file already exists, then a notification is printed.
+
+        """
+        if not path.exists():
+            with path.open(mode="w", encoding=ENCODING, newline="\n") as file:
+                file.write(temp)
+        else:
+            LOGGER.debug(f"File {path} already exists.")
