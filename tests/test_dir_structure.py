f86ba1b5b1fb0368f295f39cb67b5b13a2cc5fb7
Julian Stang <julian.stang@tum.de>
2021-04-28 07:50:57 +0200

    Added unittest for dir_structure

---
+++ tests/test_dir_structure.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+
+class TestChapter(unittest.TestCase):
+    def setUp(self) -> None:
+
+        return super().setUp()
+
+
+if __name__ == "__main__":
+    unittest.main()
