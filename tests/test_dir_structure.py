049252f5555c4525641ed7ee64f97f514ab62370
Julian Stang <julian.stang@tum.de>
2021-05-21 14:59:02 +0200

    Foundation for tests

--- tests/test_dir_structure.py
+++ tests/test_dir_structure.py
@@ -5,8 +5,19 @@ import unittest
 
 class TestChapter(unittest.TestCase):
     def setUp(self) -> None:
+        pass
 
-        return super().setUp()
+    def test_savefig_matplotlib(self):
+        pass
+
+    def test_savefig_plotly(self):
+        pass
+
+    def test_savefig_plotly_eps(self):
+        pass
+
+    def test_savefig_plotly_kaleido(self):
+        pass
 
 
 if __name__ == "__main__":
